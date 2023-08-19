47a1d95abe549dae7d7ec11b0f32c6667376b728
Eyrma <erotstein@outlook,com>
2023-08-19 16:47:40 -0700

    updated

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 function newImage(url, left, bottom){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
-
-function move(image){
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
-
-    function moveToCoordinates(left, bottom) {
-        
-    }
-}
-
-let greenCharacter = newImage('assets/green-character.gif')
-move(greenCharacter to 100, 100)
-
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
 
 
 function newItem(url, left, bottom){
@@ -61,4 +38,33 @@ function newInventory(){
     return inventory
 }
 
+function move(element){
+    element.style.position = 'fixed'
+    
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+
+    }
+    return {
+            to: moveToCoordinates
+        }
+}
+
 const inventory = newInventory()
+
+let greenCharacter = newImage('assets/green-character.gif')
+let thingThatMoveReturns = move(greenCharacter)
+thingThatMoveReturns.to(100, 250)
+
+
+move(inventory).to(0, 0)
+move(newImage('assets/green-character.gif')).to(100, 250)
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
